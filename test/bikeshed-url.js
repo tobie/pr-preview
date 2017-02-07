8cdbc3a77484246e32e9747515cc02a93ad50c29
Tobie Langel <tobie@codespeaks.com>
2017-02-07 22:18:09 +0100

    Add bikeshed-url modules and testing.

---
+++ test/bikeshed-url.js
@@ -0,0 +1,24 @@
+var assert = require('assert'),
+    bikeshedUrl = require('../lib/bikeshed-url');
+const fixtures = require("./fixtures/pr");
+
+suite('Bikeshed URLs', function() {
+    test('Basic URL', function() {
+        assert.equal(
+            bikeshedUrl({ src_file: "index.bs" }, fixtures.head),
+            "https://api.csswg.org/bikeshed/?url=https://raw.githubusercontent.com/tobie/webidl/7dfd134ee2e6df7fe0af770783a6b76a3fc56867/index.bs"
+        )
+    });
+    
+    test('Specific status', function() {
+        assert.equal(
+            bikeshedUrl({
+                src_file: "index.bs",
+                bikeshed_parameters: {
+                    "md-status": "REC"
+                }
+            }, fixtures.head),
+            "https://api.csswg.org/bikeshed/?url=https://raw.githubusercontent.com/tobie/webidl/7dfd134ee2e6df7fe0af770783a6b76a3fc56867/index.bs&md-status=REC"
+        )
+    });
+});
