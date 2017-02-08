9572fe67a11bb2b303126a7543d5bc1ae85a0888
Tobie Langel <tobie@codespeaks.com>
2017-02-09 00:29:38 +0100

    Add mustache templating to params.

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
 # webidl-pr-preview
-Adds preview and diff to spec pull requests.
+Adds preview and diff links to pull requests.
 
 ## Assumptions
 
-Currently, webidl-pr-preview makes the following assumptions about the structure of your repository:
-
-* You are using Bikeshed to build your spec and PRs contain the rendered bs file at `index.html`.
+Th eonly assumption made by webidl-pr-preview is that you're using Bikeshed to edit your spec.
 
 ## Configuration
 
 You can configure webidl-pr-preview by adding a
 `.pr-preview.json` json file at the root of your repository
 with the following fields:
-
 
 ```json
 {
@@ -26,7 +23,34 @@ with the following fields:
 
 webidl-pr-preview uses the following defaults:
 
-* **ed_url**: `https://[OWNER_NAME].github.io/[REPO_NAME]/`
-* **src_file**: `index.bs`
-* **bikeshed_parameters**: `{ }`
+```json
+{
+    "ed_url": "https://[OWNER_LOGIN].github.io/[REPO_NAME]/",
+    "src_file": "index.bs",
+     "bikeshed_parameters": { }
+}
+```
+
+### Bikeshed params
+
+`bikeshed_parameters` are used to construct the URL that transform the source file into an html document
+using [Bikeshed's web service](https://api.csswg.org/bikeshed/).
+
+When constructing the URL, `bikeshed_parameters` are rendered as if they were [mustache template strings](https://github.com/janl/mustache.js#mustachejs---logic-less-mustache-templates-with-javascript).
+
+They're passed an object containing the `config` object itself,
+the [`pull_request` payload](https://developer.github.com/v3/pulls/#get-a-single-pull-request)
+and the `owner`, `repo`, `branch`, `sha` and `short_sha` of the branch being rendered:
+
+```js
+{
+    config: { /* ... */ },       // The config object defined above.
+    pull_request: { /* ... */ }, // The pull request payload.
+    owner: "heycam",             // owner's login
+    repo: "webidl",              // repo's name
+    branch: "gh-pages",          // branch name
+    sha: "7dfd134ee2e6df7fe0..." // duh
+    short_sha: "7dfd134"         // sha truncated to 7 characters
+}
+```
 
--- test/bikeshed-url.js
+++ test/bikeshed-url.js
@@ -3,29 +3,73 @@ var assert = require('assert'),
 const fixtures = require("./fixtures/pr").pull_request;
 
 suite('Bikeshed URLs', function() {
+    var URL = "https://api.csswg.org/bikeshed/?url=https%3A%2F%2Fraw.githubusercontent.com%2Ftobie%2Fwebidl%2F7dfd134ee2e6df7fe0af770783a6b76a3fc56867%2F";
     test('Basic URL', function() {
         assert.equal(
-            bikeshedUrl({ src_file: "index.bs" }, fixtures.head),
-            "https://api.csswg.org/bikeshed/?url=https%3A%2F%2Fraw.githubusercontent.com%2Ftobie%2Fwebidl%2F7dfd134ee2e6df7fe0af770783a6b76a3fc56867%2Findex.bs"
+            bikeshedUrl({
+                config: { src_file: "index.bs" },
+                pull_request: fixtures,
+                owner:        fixtures.head.repo.owner.login,
+                repo:         fixtures.head.repo.name,
+                branch:       fixtures.head.ref,
+                sha:          fixtures.head.sha,
+                short_sha:    fixtures.head.sha.substr(0, 7)
+            }),
+            URL + "index.bs"
         )
     });
     
     test('Basic URL with non standard src file name', function() {
         assert.equal(
-            bikeshedUrl({ src_file: "url.bs" }, fixtures.head),
-            "https://api.csswg.org/bikeshed/?url=https%3A%2F%2Fraw.githubusercontent.com%2Ftobie%2Fwebidl%2F7dfd134ee2e6df7fe0af770783a6b76a3fc56867%2Furl.bs"
+            bikeshedUrl({
+                config: { src_file: "url.bs" },
+                pull_request: fixtures,
+                owner:        fixtures.head.repo.owner.login,
+                repo:         fixtures.head.repo.name,
+                branch:       fixtures.head.ref,
+                sha:          fixtures.head.sha,
+                short_sha:    fixtures.head.sha.substr(0, 7)
+            }),
+            URL + "url.bs"
         )
     });
     
     test('Specific status', function() {
         assert.equal(
             bikeshedUrl({
-                src_file: "index.bs",
-                bikeshed_parameters: {
-                    "md-status": "REC"
-                }
-            }, fixtures.head),
-            "https://api.csswg.org/bikeshed/?url=https%3A%2F%2Fraw.githubusercontent.com%2Ftobie%2Fwebidl%2F7dfd134ee2e6df7fe0af770783a6b76a3fc56867%2Findex.bs&md-status=REC"
+                config: {
+                    src_file: "index.bs",
+                    bikeshed_parameters: { "md-status": "REC" }
+                },
+                pull_request: fixtures,
+                owner:        fixtures.head.repo.owner.login,
+                repo:         fixtures.head.repo.name,
+                branch:       fixtures.head.ref,
+                sha:          fixtures.head.sha,
+                short_sha:    fixtures.head.sha.substr(0, 7)
+            }),
+            URL + "index.bs&md-status=REC"
+        )
+    });
+    
+    test('Specific status using templating', function() {
+        assert.equal(
+            bikeshedUrl({
+                config: {
+                    title: "FOO BAR",
+                    src_file: "index.bs",
+                    bikeshed_parameters: {
+                        "md-title": "{{config.title}} {{owner}}/{{repo}}/{{branch}}#{{pull_request.number}}-{{short_sha}}"
+                    }
+                },
+                pull_request: fixtures,
+                owner:        fixtures.head.repo.owner.login,
+                repo:         fixtures.head.repo.name,
+                branch:       fixtures.head.ref,
+                sha:          fixtures.head.sha,
+                short_sha:    fixtures.head.sha.substr(0, 7)
+            }),
+            URL + "index.bs&md-title=FOO%20BAR%20tobie%2Fwebidl%2Finterface-objs%23283-7dfd134"
         )
     });
 });
