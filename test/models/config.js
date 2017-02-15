ad21ee88caa76026a160f1094342965b84302c38
Tobie Langel <tobie@codespeaks.com>
2017-02-15 14:42:01 +0100

    Rewrite.

---
+++ test/models/config.js
@@ -0,0 +1,76 @@
+"use strict";
+const assert = require("assert"),
+    Config = require("../../lib/models/config");
+
+suite("Config validation", function() {
+
+    test("empty object", function() {
+        assert.throws(_ => Config.validate({}));
+    });
+    
+    test("null/undef", function() {
+        assert.throws(_ => Config.validate());
+        assert.throws(_ => Config.validate(null));
+    });
+    
+    test("inccorect type", function() {
+        assert.throws(_ => Config.validate({
+            src_file: "foo.bs",
+            type: "test"
+        }));
+    });
+    
+    test("correct type, no params", function() {
+        assert.doesNotThrow(_ => Config.validate({
+            src_file: "foo.bs",
+            type: "respec"
+        }));
+        
+        assert.doesNotThrow(_ => Config.validate({
+            src_file: "foo.bs",
+            type: "ReSpec"
+        }));
+        
+        assert.doesNotThrow(_ => Config.validate({
+            src_file: "foo.bs",
+            type: "bikeshed"
+        }));
+        
+        assert.doesNotThrow(_ => Config.validate({
+            src_file: "foo.bs",
+            type: "Bikeshed"
+        }));
+    });
+    
+    test("correct type, empty params", function() {
+        assert.doesNotThrow(_ => Config.validate({
+            src_file: "foo.bs",
+            type: "respec",
+            params: {}
+        }));
+    });
+    
+    test("correct type, primitive params", function() {
+        assert.doesNotThrow(_ => Config.validate({
+            src_file: "foo.bs",
+            type: "respec",
+            params: {
+                "foo": 123,
+                "bar": true,
+                "foobar": "a string",
+                "baz": null
+            }
+        }));
+    });
+    
+    test("correct type, forbidden params", function() {
+        assert.throws(_ => Config.validate({
+            src_file: "foo.bs",
+            type: "respec",
+            params: {
+                "foo": {}
+            }
+        }));
+    });
+});
+
