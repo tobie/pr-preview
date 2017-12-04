5c72778476abeab8cef2b1a2838fe1ef9ce3e8bc
Tobie Langel <tobie@codespeaks.com>
2017-12-04 10:03:01 +0100

    Improve diff URLs

--- test/models/spec-diff.js
+++ test/models/spec-diff.js
@@ -35,13 +35,13 @@ suite("SpecDiff model", function() {
         pr.payload = payload.pull_request;
         pr.merge_base_sha = "c2fd513de33ea3f053495f4e135614db6e6aa617";
         let sp = new File(pr).diff;
-        assert.equal(sp.key, "heycam/webidl/c2fd513...tobie:7dfd134.html");
+        assert.equal(sp.key, "heycam/webidl/283/c2fd513...tobie:7dfd134.html");
         
         pr = new PR("heycam/webidl/283", { id: 234 });
         pr.payload = payload2.pull_request;
         pr.merge_base_sha = "c2fd513de33ea3f053495f4e135614db6e6aa617";
         sp = new File(pr).diff;
-        assert.equal(sp.key, "heycam/webidl/c2fd513...7dfd134.html");
+        assert.equal(sp.key, "heycam/webidl/283/c2fd513...7dfd134.html");
     });
     
     test("Test key getter for specific page names", function() {
@@ -49,6 +49,6 @@ suite("SpecDiff model", function() {
         pr.payload = payload.pull_request;
         pr.merge_base_sha = "c2fd513de33ea3f053495f4e135614db6e6aa617";
         let sp = new File(pr, "foo.html").diff;
-        assert.equal(sp.key, "heycam/webidl/c2fd513...tobie:7dfd134/foo.html");
+        assert.equal(sp.key, "heycam/webidl/283/c2fd513...tobie:7dfd134/foo.html");
     });
 });
