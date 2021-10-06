dc3d947749d012c2df4486cf1b8abdf337b6b7d5
Jeremy Roman <jbroman@chromium.org>
2021-10-06 15:55:45 -0400

    wip

--- test/models/spec-diff.js
+++ test/models/spec-diff.js
@@ -7,48 +7,47 @@ const assert = require("assert"),
 const payload = require("../fixtures/pr");
 const payload2 = require("../fixtures/pr2");
 
+function prFixture(config = {}) {
+    let pr = new PR("heycam/webidl/283", { id: 234 });
+    pr.payload = payload.pull_request;
+    pr.merge_base_sha = "c2fd513de33ea3f053495f4e135614db6e6aa617";
+    let specConfig = {type: "bikeshed", src_file: "foo.bs", ...config};
+    pr.config = {specs: [specConfig]};
+    return pr;
+}
+
 suite("SpecDiff model", function() {
     test("Test constructor", function() {
-        let pr = new PR("heycam/webidl/283", { id: 234 });
-        pr.payload = payload.pull_request;
-        pr.merge_base_sha = "c2fd513de33ea3f053495f4e135614db6e6aa617";
-        let sp = new File(pr).diff;
+        let pr = prFixture();
+        let sp = new File(pr, pr.specs[0]).diff;
         assert(sp instanceof SpecDiff);
     });
     
     test("Test in_same_repo getter", function() {
-        let pr = new PR("heycam/webidl/283", { id: 234 });
-        pr.payload = payload.pull_request;
-        pr.merge_base_sha = "c2fd513de33ea3f053495f4e135614db6e6aa617";
-        let sp = new File(pr).diff;
+        let pr = prFixture();
+        let sp = new File(pr, pr.specs[0]).diff;
         assert.equal(sp.in_same_repo, false);
         
-        pr = new PR("heycam/webidl/283", { id: 234 });
+        pr = prFixture();
         pr.payload = payload2.pull_request;
-        pr.merge_base_sha = "c2fd513de33ea3f053495f4e135614db6e6aa617";
-        sp = new File(pr).diff;
+        sp = new File(pr, pr.specs[0]).diff;
         assert(sp.in_same_repo);
     });
     
     test("Test key getter", function() {
-        let pr = new PR("heycam/webidl/283", { id: 234 });
-        pr.payload = payload.pull_request;
-        pr.merge_base_sha = "c2fd513de33ea3f053495f4e135614db6e6aa617";
-        let sp = new File(pr).diff;
+        let pr = prFixture();
+        let sp = new File(pr, pr.specs[0]).diff;
         assert.equal(sp.key, "heycam/webidl/283/c2fd513...tobie:7dfd134.html");
         
-        pr = new PR("heycam/webidl/283", { id: 234 });
+        pr = prFixture();
         pr.payload = payload2.pull_request;
-        pr.merge_base_sha = "c2fd513de33ea3f053495f4e135614db6e6aa617";
-        sp = new File(pr).diff;
+        sp = new File(pr, pr.specs[0]).diff;
         assert.equal(sp.key, "heycam/webidl/283/c2fd513...7dfd134.html");
     });
     
     test("Test key getter for specific page names", function() {
-        let pr = new PR("heycam/webidl/283", { id: 234 });
-        pr.payload = payload.pull_request;
-        pr.merge_base_sha = "c2fd513de33ea3f053495f4e135614db6e6aa617";
-        let sp = new File(pr, "foo.html").diff;
+        let pr = prFixture();
+        let sp = new File(pr, pr.specs[0], "foo.html").diff;
         assert.equal(sp.key, "heycam/webidl/283/c2fd513...tobie:7dfd134/foo.html");
     });
 });
