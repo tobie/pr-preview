tobie/pr-preview

commit 42c3a5083b66e709626850af07e93df25386106c

Fix URL object being sent to error message (#155)
Fixes #154

lib/fetch-url.js
@@ -3,15 +3,15 @@
 const stripAnsi = require("strip-ansi");
 const request = require("request");
 
-module.exports = (url, service) => {
+module.exports = (urlObj, service) => {
     return new Promise((resolve, reject) => {
-        if (typeof url == "string") {
-            url = { url };
+        if (typeof urlObj == "string") {
+            urlObj = { urlObj };
         }
-        url.headers = { "User-Agent": "pr-preview" };
-        request(url, function (error, response, body) {
+        urlObj.headers = { "User-Agent": "pr-preview" };
+        request(urlObj, function (error, response, body) {
             if (error) {
-                error.data = { request_url: url, service: service };
+                error.data = { request_url: urlObj.url, service: service };
                 reject(error);
             } else if (response.statusCode >= 400) {
                 var err = new Error(`${ response.statusCode } ${ response.statusMessage }`);
@@ -20,7 +20,7 @@ module.exports = (url, service) => {
                 } catch (e) {
                     err.data = { error: stripAnsi(body) };
                 }
-                err.data.request_url = url;
+                err.data.request_url = urlObj.url;
                 err.data.service = service;
                 reject(err);
             } else {

commit bd5207e18d4f298608ca39d100efb76bea7c2e04

Fix bug introduced in #155
Closes #156

lib/fetch-url.js
@@ -6,7 +6,7 @@ const request = require("request");
 module.exports = (urlObj, service) => {
     return new Promise((resolve, reject) => {
         if (typeof urlObj == "string") {
-            urlObj = { urlObj };
+            urlObj = { url: urlObj };
         }
         urlObj.headers = { "User-Agent": "pr-preview" };
         request(urlObj, function (error, response, body) {